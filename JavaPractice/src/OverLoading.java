1bc8536fcfc59040d82b6698d7ca0b2ba4043170
sudheer <Sudheer.Villuri@gmail.com>
2020-02-18 17:08:46 +0530

    ssda

--- JavaPractice/src/OverLoading.java
+++ JavaPractice/src/OverLoading.java
@@ -1,27 +1,24 @@
 class Addition {
-
-	// adding three integer values.
-	public int add(int a, int b, int c) {
-
-		int sum = a + b + c;
-		return sum;
-	}
-
-	public int add(long a, int b) {
-		System.out.println("From here");
-		return (int) (a + b);
-	}
-
-	// adding two integer values.
-	public int add(int a, int b) {
-
-		int sum = a + b;
-		return sum;
-	}
 	
-	public String add(String a, String b) {
+	//Overloaded methods are differentiated based on the number and type of the parameters passed as an arguments to the methods.
+	//You can not define more than one method with the same name, Order and the type of the arguments. It would be compiler error.
+	//The compiler does not consider the return type while differentiating the overloaded method. But you cannot declare two methods with the same signature and different return type. It will throw a compile time error.
+	//If both methods have same parameter types, but different return type, then it is not possible. (Java SE 8 Edition, §8.4.2)
+	
+	//Overloading with different arg
+	public int add(int a,int b) {
 		return a+b;
 	}
+	
+	public int add(double a,double b) {
+		return (int) (a+b);
+	}
+	
+	//Same arg cannot have diff return type
+	public double add(int a,double b) {
+		return a+b;
+	}
+	
 }
 
 class OverLoading {
